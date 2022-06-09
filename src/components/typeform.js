39b927368f4eb8bd88f11bbefc1867c410c87583
amu <choejihye@Amu-2.local>
2022-06-09 15:12:41 +0900

    typeform updated

--- src/components/typeform.js
+++ src/components/typeform.js
@@ -1,8 +1,14 @@
-import { Widget } from '@typeform/embed-react'
+import { Widget, PopupButton } from '@typeform/embed-react'
 import * as React from "react";
 
-const MyComponent = () => {
-  return <Widget id="cGMw88rA" style={{ width: '100%', height: '400px' }} className="my-form" />
+const Popup = () => {
+  return (
+    <PopupButton id="cGMw88rA" style={{ fontSize: 20 }} className="button">
+      문의하기 
+    </PopupButton>
+  )
 }
 
-export default MyComponent;
+
+
+export default Popup;
