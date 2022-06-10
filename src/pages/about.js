1fb01e79d6745c90dd728a37578906badf4557c6
amu <choejihye@Amu-2.local>
2022-06-10 12:13:36 +0900

    service page +

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,42 +5,55 @@ import { Link } from "gatsby";
 import { Grid, Row, Col, Cell } from "react-styled-grid-layout";
 import ReactRoundedImage from "react-rounded-image";
 import Photo from "../images/profile_removebg.png";
-import { GiSprout } from 'react-icons/gi';
-
-
+import { GiSprout } from "react-icons/gi";
 
 // Step 2: Define your component
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
-     
-      <h2> <div className = "icon">< GiSprout /></div>&nbsp;&nbsp;대표 인사말</h2>
-<div className="Blank"></div>
+      <h2>
+        {" "}
+        <div className="icon">
+          <GiSprout />
+        </div>
+        &nbsp;&nbsp;대표 인사말
+      </h2>
+      <div className="Blank"></div>
 
-
-<Grid cols="12" gap="10px">
- 
-          <Col xxl="4" md="6" sm="12">
-      <div align= "center">   
-      <ReactRoundedImage
-
-          image={Photo}
-          roundedColor="#fff"
-          imageWidth="150"
-          imageHeight="150"
-          roundedSize="2"
-          borderRadius="90"
-        /></div>
-      </Col>
-    <Col xxl="8" md="6" sm="12">
-       
-      <p>
-        안녕하세요. <b>현직한의사</b>이자 모든마케팅 대표 안상완입니다.
-      </p>
-      </Col>
+      <Grid cols="12" gap="10px">
+        <Col xxl="4" md="6" sm="12">
+          <div align="center">
+            <ReactRoundedImage
+              image={Photo}
+              roundedColor="#fff"
+              imageWidth="150"
+              imageHeight="150"
+              roundedSize="2"
+              borderRadius="90"
+            />
+          </div>
+        </Col>
+        <Col xxl="8" md="6" sm="12">
+          <p>
+            안녕하세요. <b>현직한의사</b>이자 모든마케팅 대표 <b>안상완</b>입니다.
+          </p>
+          <p>
+            모든마케팅 임직원은 끊임없이 배우고 익히면서 고객사 분들이
+            더 많은 고객과 접점을 만들어 갈 수 있게 노력하고 있습니다.
+          </p>
+          <p>
+            기존에 강점을 가진 부분들을 잘 유지하면서 최근 트렌드인 그로스 /
+            애자일 쪽 정보를 업데이트하여 변화해가는 비즈니스 환경에 기민하게
+            대응하려 합니다.
+          </p>
+          <p>
+            현실적인 조건하에서 ROI가 높은 선택지를 제시하여 고객사의 합리적인
+            마케팅 의사결정을 돕겠습니다.
+          </p>
+          <p>감사합니다.</p>
+        </Col>
       </Grid>
-<div className="Blank"></div>
-
+      <div className="Blank"></div>
     </Layout>
   );
 };
--- src/pages/service/blog.js
+++ src/pages/service/blog.js
@@ -12,7 +12,7 @@ import { AnchorLink } from "gatsby-plugin-anchor-links";
 
 const SuperStrong = ({ children }) => {
   return (
-    <strong style={{ fontSize: "55px", fontFamily: "Pretendard Thin" }}>
+    <strong style={{ fontSize: "45px", fontFamily: "Pretendard Thin" }}>
       {children}
     </strong>
   );
@@ -37,8 +37,7 @@ const AboutPage = () => {
         <Col xxl="4" md="6" sm="12">
           <div>
             <h2>
- 
-             <TypeIt
+              <TypeIt
                 options={{
                   waitUntilVisible: true,
                 }}
@@ -50,30 +49,73 @@ const AboutPage = () => {
         </Col>
         <Col xxl="8" md="6" sm="12">
           <p>
-            마케팅 대행사의 기본 관행에서 벗어나 <span className = "HL">파괴적인 가격</span>과 효율로
-            도와드리겠습니다. 모든마케팅에서 유일무이, 마케팅 효율의 끝을 경험해
-            보세요!
+            마케팅 대행사의 기본 관행에서 벗어나{" "}
+            <span className="HL">파괴적인 가격</span>과 효율로 도와드리겠습니다.
+            모든마케팅에서 유일무이, <b>마케팅 효율의 끝</b>을 경험해 보세요!
           </p>
           <div className="Blank"></div>
         </Col>
         <Col xxl="4" md="6" sm="12">
           <div>
             <h2>
-              {" "}
               <TypeIt
                 options={{
                   waitUntilVisible: true,
                 }}
               >
-                압도적인 물량!
-              </TypeIt>{" "}
+                비교불가 절대우위
+              </TypeIt>
             </h2>
           </div>
         </Col>
         <Col xxl="8" md="6" sm="12">
-          <p> 업계 평균 단가 대비 8~13만원 저렴합니다</p>
-          <p>업계 평균 포스팅수 대비 4~6배 많습니다</p>
-          <p>업계 평균 마케팅비용 대비 28% 이상 낮습니다 </p>
+          <div className="tablecell">
+            <p>
+              까놓고 말해 <b>'이 구역 미친놈은 나야'</b> 정신으로 해야 마케팅
+              캠페인이 성공할 수 있습니다. 저희는 클라이언트를 위해 기꺼이
+              미친놈이 되겠습니다. 고퀄리티 포스팅의 대량물량 폭탄 투하로 지역내
+              마케팅 경쟁에서 압도적인 우위를 점하는 전략을 완성합니다.
+              모든마케팅과 함께라면&nbsp;
+              <span className="HL">우리지역 1등 병원</span>은 바로 당신입니다.
+            </p>
+
+            <div className="details"></div>
+            <div className="details">
+              <details>
+                <summary className="buttontypetext">
+                  &nbsp;압도적인 포스팅당 비용
+                </summary>
+                <p className="subdetails">
+                  유례 없는 1건당 만오천원의 파격적인 단가에, 기존 마케팅
+                  대행사가 제공하는 포스팅 물량의 4배 이상 공급 드릴 것을
+                  약속드립니다.
+                </p>
+              </details>
+            </div>
+            <div className="details">
+              <details>
+                <summary className="buttontypetext">
+                  &nbsp;압도적인 물량
+                </summary>
+                <p className="subdetails">
+                  '이 새끼들 미친새끼들인가' 싶은 물량 폭탄으로 지역내
+                  검색수요를 장악합니다.
+                </p>
+              </details>
+            </div>
+            <div className="details">
+              <details>
+                <summary className="buttontypetext">
+                  &nbsp;압도적인 가성비
+                </summary>
+                <p className="subdetails">
+                  모든마케팅의 서비스는 인하우스 직원 여럿을 두고 직접 운영해도
+                  내기 어려운 효율을 자랑합니다. 직원 한명 비용으로 상당한
+                  규모의 마케팅팀을 운영하는 효과를 내드립니다.
+                </p>
+              </details>
+            </div>
+          </div>
           <div className="Blank"></div>
         </Col>
         <Col xxl="4" md="6" sm="12">
@@ -92,75 +134,16 @@ const AboutPage = () => {
         </Col>
         <Col xxl="8" md="6" sm="12">
           <p>
-            모든마케팅의 마케팅 기법은 <span className = "HL">업계를 선도</span>하고 있습니다. 트렌드에 민감한
-            일부 경쟁업체들은 모든마케팅의 마케팅 기법을 벤치마킹하고 있습니다.
-            업계에서 인정받는 것은 물론이거니와 지속적인 실험을 통해 마케팅
-            효율의 극한을 위해 노력합니다. 마케팅을 제법 아는 사람들이 먼저
-            알아보는 모든마케팅과 함께 성장하세요..
+            모든마케팅의 마케팅 기법은 <span className="HL">업계를 선도</span>
+            하고 있습니다. 트렌드에 민감한 일부 경쟁업체들은 모든마케팅의 마케팅
+            기법을 벤치마킹하고 있습니다. 업계에서 인정받는 것은 물론이거니와
+            지속적인 실험을 통해 마케팅 효율의 극한을 위해 노력합니다. 마케팅
+            <b> 꾼들이 먼저 알아보는</b> 모든마케팅과 함께 성장하세요.
           </p>
-          <div className="Blank"></div>
-        </Col>
-        <Col xxl="4" md="6" sm="12">
-          <div>
-            <h2>
-              {" "}
-              <TypeIt
-                options={{
-                  waitUntilVisible: true,
-                }}
-              >
-                상황에 따라 골라쓰는
-              </TypeIt>
-            </h2>
-          </div>
-        </Col>
-        <Col xxl="8" md="6" sm="12">
-          <div>
-            <details>
-              <summary>다른 대행사를 활용중인 경우</summary>
-              <p>
-                객관적인 견적 비교를 해보세요. 이미 마케팅을 하고 있거 마케팅에
-                대한 이해가 어느 정도 있다면 모든마케팅을 안할 이유를 찾기
-                어렵습니다.더 낮은 가격에 더 많은 포스팅, 더 나은 효과를 기대할
-                수 있습니다
-              </p>
-            </details>
-            <details>
-              <summary>마케팅 경험이 없고 이제 시작하려는 경우</summary>
-              <p>
-                좋은 대행사를 만나는 것이 첫 단추가 됩니다. 마케팅에 대한 전체
-                그림을 그리실수 있게 마케팅전략부터 친절히 상담드리겠습니다.
-              </p>
-            </details>
-            <p className = "subtext">
-              마케팅 경험 또는 이해도에 따라 <span className = "HL">맞춤식 컨설팅</span>을 해드릴 예정이니,
-              하단의 상담신청 파트를 참고 부탁드립니다.
-            </p>
-            <div>
-              <AnchorLink to="#contact">
-                {" "}
-                <button className="button">상담신청하기</button>
-              </AnchorLink>
-            </div>
-          </div>
-          <div className="Blank"></div>
-        </Col>
-        <Col xxl="4" md="6" sm="12">
-          <div>
-            <h2>
-              {" "}
-              <TypeIt
-                options={{
-                  waitUntilVisible: true,
-                }}
-              >
-                마케팅 방식이 궁금해요{" "}
-              </TypeIt>
-            </h2>
-          </div>
-        </Col>
-        <Col xxl="8" md="6" sm="12">
-          <p>자세한 내용은 칼럼 글을 참고해주세요.</p>
+          <p>
+            저희의 마케팅 철학과 구체적인 방법론이 궁금하신 분들은 칼럼 글을
+            참고해주세요.
+          </p>
           <div>
             <button className="button">
               <div className="icon">
@@ -169,7 +152,76 @@ const AboutPage = () => {
               &nbsp;&nbsp;칼럼 보러가기
             </button>
           </div>
+          <div className="Blank"></div>
+        </Col>
+        <Col xxl="4" md="6" sm="12">
+          <div>
+            <h2>
+              <TypeIt
+                options={{
+                  waitUntilVisible: true,
+                }}
+              >
+                상황에 따라 골라쓰세요
+              </TypeIt>
+            </h2>
+          </div>
+        </Col>
+        <Col xxl="8" md="6" sm="12">
+          <div className="tablecell">
+            <div className="details"></div>
+            <div className="details">
+              <details>
+                <summary className="buttontypetext">
+                  &nbsp;다른 대행사를 활용중인 경우
+                </summary>
+                <p className="subdetails">
+                  이미 마케팅을 하고 있다면 객관적으로 견적 비교를 해보세요.
+                  마케팅에 대한 이해가 어느 정도 있다면 모든마케팅을 안할 이유를
+                  찾기 어렵습니다. 더 낮은 가격에 더 많은 포스팅, 더 나은 효과를
+                  기대할 수 있습니다.
+                </p>
+              </details>
+            </div>
+            <div className="details">
+              <details>
+                <summary className="buttontypetext">
+                  &nbsp;마케팅 경험이 없는 경우
+                </summary>
+                <p className="subdetails">
+                  좋은 대행사를 만나는 것이 첫 단추가 됩니다. 마케팅에 대한 전체
+                  그림을 그리실 수 있게 마케팅전략부터 구체적인 플랜까지 친절히
+                  상담드리겠습니다.
+                </p>
+              </details>
+            </div>
+            <div className="details">
+              <details>
+                <summary className="buttontypetext">
+                  &nbsp;프랜차이즈 본사인 경우
+                </summary>
+                <p className="subdetails">
+                  여러 가맹점의 마케팅을 전담해야 하는 프랜차이즈 본사라면,
+                  모든마케팅이 실행사 레벨에서 서포트를 해드립니다. 체계적인
+                  포스팅 리포트 및 노출현황 모니터링을 통해 지점별 관리를
+                  효율적으로 진행할 수 있습니다. 기타 특별한 서비스가 필요하다면
+                  저희에게 제안해주셔도 좋습니다. 
+                </p>
+              </details>
+            </div>
 
+            <p>
+              마케팅 경험 또는 이해도에 따라{" "}
+              <span className="HL">맞춤형 컨설팅</span>을 해드릴 예정이니,
+              궁금한 사항은 하단의 상담신청 버튼을 활용해주세요.
+            </p>
+            <div>
+              <AnchorLink to="#contact">
+                {" "}
+                <button className="button">상담신청하기</button>
+              </AnchorLink>
+            </div>
+          </div>
           <div className="Blank"></div>
         </Col>
 
