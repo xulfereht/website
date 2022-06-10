45bd023790a5c916c445243164ac7bae462bf64a
amu <choejihye@192.168.nate.com>
2022-06-10 20:40:04 +0900

    blog template

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,33 +1,38 @@
-import * as React from 'react'
-import { Link, graphql } from 'gatsby'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
-import Layout from '../../components/layout'
+import * as React from "react";
+import { Link, graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+import Layout from "../../components/layout";
+import { Grid, Row, Col, Cell } from "react-styled-grid-layout";
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
+      <Grid cols="12" gap="10px">
+        <Col xxl="4" md="6" sm="12">
+          <h2>전문가 칼럼</h2>
+        </Col>
+    
+        <Col xxl="8" md="6" sm="12">      <p>칼럼에는 우리의 마케팅철학과 전략을 소개합니다</p>
       <ul>
-      {
-        data.allMdx.nodes.map(node => (
+        {data.allMdx.nodes.map((node) => (
           <article key={node.id}>
-            <h2>
-            <Link to = {`/blog/${node.slug}`}>
-            {node.frontmatter.title}
-            </Link>
-            </h2>
-            <p>Posted: {node.frontmatter.date}</p>
+              <Link to={`/blog/${node.slug}`}>
+                <div className = "blogCard">{node.frontmatter.title}</div>
+              </Link>
           </article>
-        ))
-
-      }
+        ))}
       </ul>
+</Col>
+      </Grid>
+
+      <div className="Blank"></div>
     </Layout>
-  )
-}
+  );
+};
 
 export const query = graphql`
   query {
-    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
       nodes {
         frontmatter {
           date(formatString: "MMMM D, YYYY")
@@ -38,5 +43,5 @@ export const query = graphql`
       }
     }
   }
-`
-export default BlogPage
+`;
+export default BlogPage;
--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,22 +1,30 @@
-import * as React from 'react'
-import { graphql } from 'gatsby'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
-import Layout from '../../components/layout'
+import * as React from "react";
+import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+import Layout from "../../components/layout";
+import { Grid, Row, Col, Cell } from "react-styled-grid-layout";
 
 const BlogPost = ({ data }) => {
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
-      <p>{data.mdx.frontmatter.date}</p>
-      <MDXRenderer>
-        {data.mdx.body}
-      </MDXRenderer>
+
+      <Grid cols="12" gap="10px">
+        <Col xxl="4" md="6" sm="12">
+          <h2>{data.mdx.frontmatter.title}</h2>
+          <p className="subtext">{data.mdx.frontmatter.date}</p>
+        </Col>
+        <Col xxl="8" md="6" sm="12">
+          <MDXRenderer>{data.mdx.body}</MDXRenderer>
+        </Col>
+        <div className="Blank"></div>
+      </Grid>
     </Layout>
-  )
-}
+  );
+};
 
 export const query = graphql`
   query ($id: String) {
-    mdx(id: {eq: $id}) {
+    mdx(id: { eq: $id }) {
       frontmatter {
         title
         date(formatString: "MMMM D, YYYY")
@@ -24,6 +32,6 @@ export const query = graphql`
       body
     }
   }
-`
+`;
 
-export default BlogPost
+export default BlogPost;
