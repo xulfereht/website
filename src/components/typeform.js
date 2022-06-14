1a6a8ae75dabf8cf6a8464ba403b805dac4860f6
amu <choejihye@Amu-2.local>
2022-06-14 16:01:51 +0900

    contact us link change

--- src/components/typeform.js
+++ src/components/typeform.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Widget } from '@typeform/embed-react'
 
 const MyComponent = () => {
-  return <Widget id="cGMw88rA" style={{ width: '100%', height: '400px' }} className="my-form" />
+  return <Widget id="sS9ViGNy" style={{ width: '100%', height: '400px' }} className="my-form" />
 }
 
 
