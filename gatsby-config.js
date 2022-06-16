13edd3a499ffadec0432d958e564ed33d52ab0e3
amu <choejihye@Amu-2.local>
2022-06-16 21:08:38 +0900

    cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,29 +11,7 @@ module.exports = {
     "gatsby-plugin-sitemap",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 630,
-            },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-        ],
-      },
-    },
-
+    `gatsby-transformer-remark`,
     {
       resolve: "gatsby-plugin-robots-txt",
       options: {
