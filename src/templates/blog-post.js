298629996484b725423301a6e2f98db2375042da
amu <choejihye@Amu-2.local>
2022-06-17 16:32:43 +0900

    cms

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -14,19 +14,45 @@ const BlogPostTemplate = ({ data, location }) => {
         <Col xxl="1" md="6" sm="12"></Col>
 
         <Col xxl="10" md="6" sm="12">
-          <h1 className = "blogTitle">{post.frontmatter.title}</h1>
-          <p className = "subtext"><span className = "blogSub"><b>모든마케팅</b></span> · {post.frontmatter.date}</p>
-
+          <h1 className="blogTitle">{post.frontmatter.title}</h1>
+          <p className="subtext">
+            <span className="blogSub">
+              <b>모든마케팅</b>
+            </span>{" "}
+            · {post.frontmatter.date}
+          </p>
           {post.frontmatter.tags.map((tag) => (
             <Link to={`/tags/${tag}`} className="tagbtn">
               {tag}
             </Link>
           ))}
-            <div className = "topBlank"></div>
+          <div className="topBlank"></div>
           <section
             dangerouslySetInnerHTML={{ __html: post.html }}
             itemProp="articleBody"
           />
+          <div className="topBlank"></div>
+
+          <div>
+            {next ? (
+              <div className="blogNav">
+                <Link to={next.fields.slug}>
+                  Next : {next.frontmatter.title}
+                </Link>
+              </div>
+            ) : (
+              <div></div>
+            )}
+            {previous ? (
+              <div className="blogNav">
+                <Link to={previous.fields.slug}>
+                  Prev : {previous.frontmatter.title}
+                </Link>
+              </div>
+            ) : (
+              <div></div>
+            )}
+          </div>
         </Col>
         <Col xxl="1" md="6" sm="12"></Col>
 
