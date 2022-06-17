0b288960b89bbfcca0ea6a09f620e613f48538f8
amu <choejihye@Amu-2.local>
2022-06-17 13:28:41 +0900

    cms

--- src/pages/service/guide-for-betatester.js
+++ src/pages/service/guide-for-betatester.js
@@ -20,7 +20,6 @@ const AboutPage = () => {
       <div className="divmargin">
         <h2> 🌈  다양해진 키워드 스펙트럼</h2>
 
-        <p>
           <ul>
             <li>무료체험 : 3지역 x 3키워드 조합으로 최대 9개 키워드</li>
             <li>유료서비스 : 권역내 유효키워드 전체(1,000개 이상)</li>
@@ -49,7 +48,6 @@ const AboutPage = () => {
           </ul>
           롱테일키워드에 대해서는 아래 글을 참고해주세요.
           <br></br>
-        </p>
 
         <div>
           <Link to="/로컬비지니스와-롱테일마케팅">
@@ -59,7 +57,6 @@ const AboutPage = () => {
       </div>
       <div className="divmargin">
         <h2> 👍  비교를 불허하는 압도적인 물량 </h2>
-        <p>
           <ul>
             <li>
               폭탄블로그 일반형의 월간 약속물량은 <b>120건</b>입니다.
@@ -78,12 +75,11 @@ const AboutPage = () => {
               자부합니다.
             </li>
           </ul>
-        </p>
       </div>
       <div className="divmargin">
         <h2> 📈  편의성을 높인 리포트 시스템 </h2>
         <p>
-          마케팅 집행 현황을 <b>인트라넷을 통해 확인</b>하실수 있습니다.{" "}
+          마케팅 집행 현황을 <b>인트라넷을 통해 확인</b>하실수 있습니다.{" "}</p>
           <br></br>
           <ul>
             <li>
@@ -103,13 +99,13 @@ const AboutPage = () => {
               주요 마케팅 상황에 대한 <b>리포트가 문자로 발송</b>됩니다.
             </li>
           </ul>
-        </p>
+        
       </div>
       <div className="divmargin">
         <h2> 🚀  포스팅 방식의 변경 </h2>
         <p>
           모든마케팅의 유료서비스에서는 두 종류의 계정을 활용해 포스팅을
-          진행합니다.
+          진행합니다.</p>
           <ul>
             <li>자체 보유 블로그계정 </li>
             <li>클라이언트가 제공한 블로그계정</li>{" "}
@@ -118,7 +114,7 @@ const AboutPage = () => {
           가능합니다. 블로그계정은 가급적이면 새로 생성하셔서 주시는게 좋습니다.
           이 사항은 마케팅 오퍼레이팅 차원에서 매우 중요한 내용이라 따로 별도의
           문서로 안내해드리고 있습니다. 관련해서는 아래 문서를 꼭 참고해주세요.
-        </p>
+        
         <div>
           <Link to="/블로그-아이디-만드는-방법-및-주의사항/" target="_blank">
             <button className="button">&nbsp;&nbsp;계정생성 매뉴얼</button>
@@ -127,7 +123,7 @@ const AboutPage = () => {
       </div>
       <div className="divmargin">
         <h2> 🔧  컨텐츠 이미지 변경 및 커스터마이징 </h2>
-        <p>
+        
           <ul>
             <li>
               무료체험 과정에서 포스팅에 활용된 시안은 기본 컨텐츠 템플릿으로
@@ -146,7 +142,7 @@ const AboutPage = () => {
               가능합니다(추가비용 발생).
             </li>
           </ul>
-        </p>
+        
       </div>
       <div className="divmargin">
 <h2> 💰  서비스비용 안내 </h2>
