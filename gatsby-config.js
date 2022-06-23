09e9a76cb001560c69f96bede4a76dd855bcfc9d
amu <choejihye@Amu-2.local>
2022-06-23 13:22:45 +0900

    update robottxt

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,89 +1,100 @@
 module.exports = {
-  siteMetadata: {
-    title: `모든마케팅`,
-    description:
-      "병원, 치과, 한의원, 변호사 등 전문직 마케팅 전문 모든마케팅입니다.",
-    siteUrl: `http://www.moden.marketing/`,
-  },
-  plugins: [
-    `gatsby-plugin-netlify-cms`,
-      "gatsby-plugin-webpack-bundle-analyser-v2",
-    `gatsby-plugin-react-helmet`,
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    `gatsby-transformer-remark`,
-    {
-      resolve: "gatsby-plugin-robots-txt",
-      options: {
-        host: "http://www.moden.marketing",
-        sitemap: "http://moden.markeing/sitemap.xml",
-        policy: [{ userAgent: "*", allow: "/" }],
-      },
+    siteMetadata: {
+        title: `모든마케팅`,
+        description:
+        "병원, 치과, 한의원, 변호사 등 전문직 마케팅 전문 모든마케팅입니다.",
+        siteUrl: `http://www.moden.marketing/`,
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    "gatsby-plugin-styled-components",
-    `gatsby-plugin-anchor-links`,
-    {
-      resolve: `gatsby-plugin-scroll-reveal`,
-      options: {
-        threshold: 0.5, // Percentage of an element's area that needs to be visible to launch animation
-        once: true, // Defines if animation needs to be launched once
-        disable: false, // Flag for disabling animations
+    plugins: [
+        `gatsby-plugin-netlify-cms`,
+        "gatsby-plugin-webpack-bundle-analyser-v2",
+        `gatsby-plugin-react-helmet`,
+        "gatsby-plugin-sitemap",
+        "gatsby-plugin-image",
+        "gatsby-plugin-sharp",
+        `gatsby-transformer-remark`,
+        {
+            resolve: 'gatsby-plugin-robots-txt',
+            options: {
+                host: 'https://moden.marketing',
+                sitemap: 'https://moden.marketing/sitemap/sitemap-0.xml',
+                policy: [{
+                    userAgent: '*',
+                    allow: '/'
+                }]
+            }
+        },
+        {
+            resolve: "gatsby-plugin-robots-txt",
+            options: {
+                host: "http://www.moden.marketing",
+                sitemap: "http://moden.markeing/sitemap.xml",
+                policy: [{ userAgent: "*", allow: "/" }],
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/content/blog`,
+                name: `blog`,
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        "gatsby-plugin-styled-components",
+        `gatsby-plugin-anchor-links`,
+        {
+            resolve: `gatsby-plugin-scroll-reveal`,
+            options: {
+                threshold: 0.5, // Percentage of an element's area that needs to be visible to launch animation
+                once: true, // Defines if animation needs to be launched once
+                disable: false, // Flag for disabling animations
 
-        // Advanced Options
-        selector: "[data-sal]", // Selector of the elements to be animated
-        animateClassName: "sal-animate", // Class name which triggers animation
-        disabledClassName: "sal-disabled", // Class name which defines the disabled state
-        rootMargin: "0% 50%", // Corresponds to root's bounding box margin
-        enterEventName: "sal:in", // Enter event name
-        exitEventName: "sal:out", // Exit event name
-      },
-    },
-    {
-      resolve: "gatsby-plugin-react-svg",
-      options: {
-        rule: {
-          include: /assets/, // See below to configure properly
+                // Advanced Options
+                selector: "[data-sal]", // Selector of the elements to be animated
+                animateClassName: "sal-animate", // Class name which triggers animation
+                disabledClassName: "sal-disabled", // Class name which defines the disabled state
+                rootMargin: "0% 50%", // Corresponds to root's bounding box margin
+                enterEventName: "sal:in", // Enter event name
+                exitEventName: "sal:out", // Exit event name
+            },
         },
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "G-K26075SSRD", // Google Analytics / GA
-        ],
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        {
+            resolve: "gatsby-plugin-react-svg",
+            options: {
+                rule: {
+                    include: /assets/, // See below to configure properly
+                },
+            },
         },
-      },
-    },
-    {
-      resolve: `gatsby-plugin-facebook-pixel`,
-      options: {
-        pixelId: "401592791878972",
-      },
-    },
-  ],
+        {
+            resolve: `gatsby-plugin-google-gtag`,
+            options: {
+                // You can add multiple tracking ids and a pageview event will be fired for all of them.
+                trackingIds: [
+                    "G-K26075SSRD", // Google Analytics / GA
+                ],
+                // This object is used for configuration specific to this plugin
+                pluginConfig: {
+                    // Puts tracking script in the head instead of the body
+                    head: false,
+                    // Setting this parameter is also optional
+                    respectDNT: true,
+                    // Avoids sending pageview hits from custom paths
+                    exclude: ["/preview/**", "/do-not-track/me/too/"],
+                },
+            },
+        },
+        {
+            resolve: `gatsby-plugin-facebook-pixel`,
+            options: {
+                pixelId: "401592791878972",
+            },
+        },
+    ],
 };
